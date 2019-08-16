689da1cb0cda68d432a2da7486e91d3bdc514f5a
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-08-16 10:24:45 +0530

    Create Redis.js

---
+++ redis connection/Redis.js
@@ -0,0 +1,18 @@
+//Before running we have to start the redis-server
+var redis = require("redis");
+var client = redis.createClient();
+
+client.on("connec", function(){
+	console.log("connected")
+});
+
+client.on('error', function (err) {
+    console.log('Something went wrong ' + err);
+});
+
+ client.set("key","redis")
+ client.expire("keys", 2)
+
+client.get("key",function(err,reply){
+	console.log(reply)
+})
