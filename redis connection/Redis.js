aada18610d5627b73c6fc6262a6a0b5af022225b
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-08-19 15:33:43 +0530

    Update Redis.js

--- redis connection/Redis.js
+++ redis connection/Redis.js
@@ -56,6 +56,57 @@ client.exists("key",function(err,res){
 
 //.10 type key: is used to know datatype stored in the key
 
+//.11 getrange cmd: is used to get the substring from the string value
+ 
+//.12 getset cmd: is used to set the new value and returns the old value
+
+//.13 mget cmd: is used to get the values of all spicified keys
+EX: set key1 "hello"
+    set key2 "haiii"
+    set key3 "haaaa"
+    set key4 "hoooo"
+mget key1 key2 key3 key4
+
+//.14 setex cmd: is used to set the value with a specified time 
+EX: setex key 10 "securifi embedded systems"
+
+//.15 setnx cmd: is used to set the value and that value can't be overcome 
+EX: setnx key "checking data"
+
+//.16 strlen cmd: is used to know the length of the string that means key value length
+EX: strlen key1
+
+//.17 mset cmd: is used to set the multiple key value pairs at a time 
+EX: mset key1 "one" key2 "two" key3 "three'
+
+//.18 mget cmd: is used to get the multiple key values at a time
+EX: mget key1 key2
+
+//.19 incr cmd: is used to increment the one value 
+EX: set key 100
+    incr key
+    get key ==>101
+
+//.20 incrby cmd: is used to increment the value upto our required position 
+EX: set key 200
+    incby key 50
+    get key ==>250
+
+//.21 decr cmd: is used to decrement the one value position 
+EX: set key 100
+    decr key
+    get key ==>99
+
+//.22 decrby cmd: is used to decrement the value upto our required position
+EX: set key 200
+    decrby key 25
+    get key  ==>175
+
+
+
+
+
+
 
 
 
